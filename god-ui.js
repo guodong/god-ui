68f661024e859d2fecf8449ed18ea2bd24946874
root <gd@tongjo.com>
2013-11-08 07:18:30 +0800

    add kit

--- god-ui.js
+++ god-ui.js
@@ -2,4 +2,13 @@ $(function(){
 	$(".-tabs li").click(function(){
 		$(this).addClass("-active").siblings().removeClass("-active").parent().parent().find(".-pane").hide().eq($(this).index()).show();
 	});
+	$("[gui-cmd=modal]").click(function(){
+		var id = $(this).attr("gui-arg");
+		$("body").append("<div class='-mask'></div>");
+		$("#"+id).fadeIn();
+	});
+	$("[gui-cmd=close]").click(function(){
+		$(this).parents(".-modal").fadeOut();
+		$(".-mask").remove();
+	});
 });
